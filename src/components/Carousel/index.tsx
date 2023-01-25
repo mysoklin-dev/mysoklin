419b329486d6cd7121695146cd29cd6bc254378c
azhari.subroto <azhari.subroto@gmail.com>
2023-01-25 14:27:47 +0700

    slide

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -2,19 +2,29 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a lo
 
 import React from 'react';
 import { Carousel as ReactCarousel } from 'react-responsive-carousel';
+import useSWR from 'swr';
 
 const Carousel = () => {
+  const { data, error } = useSWR(
+    `${process.env.NEXT_PUBLIC_PB_URL}/api/collections/hero_banner/records?page=1&perPage=3&sort=+sequence`
+  );
+
+  const loading = !data && !error;
+  const posts = data && data.items;
+
   return (
     <ReactCarousel showThumbs={false} autoPlay swipeable>
-      <div>
-        <img src="/assets/images/banner2-1 1.jpg" alt="test" />
-      </div>
-      <div>
-        <img src="/assets/images/banner2-1 1.jpg" alt="test" />
-      </div>
-      <div>
-        <img src="/assets/images/banner2-1 1.jpg" alt="test" />
-      </div>
+      {!loading &&
+        posts &&
+        posts.length > 0 &&
+        posts.map((item: any) => (
+          <div key={`slide-${item.id}`}>
+            <img
+              src={`${process.env.NEXT_PUBLIC_API_URL}/files/${item.collectionId}/${item.id}/${item?.banner}`}
+              alt={item.title}
+            />
+          </div>
+        ))}
     </ReactCarousel>
   );
 };
