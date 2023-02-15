d08d6551b935030e9a7a66d8c310ea5f265a5e7e
azhari.subroto <azhari.subroto@gmail.com>
2023-02-15 13:47:15 +0700

    head fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,26 +17,24 @@ const Index: NextPage<any> = ({ og }) => {
   return (
     <Main>
       <Head>
-        <Head>
-          <title>{og?.og_title}</title>
-          <meta property="og:title" content={og?.og_title} />
-          <meta
-            name="description"
-            content={og?.og_description.substring(0, 100)}
-          />
-          <meta
-            property="og:description"
-            content={og?.og_description.substring(0, 100)}
-          />
-          <meta
-            property="og:image"
-            content={`${process.env.NEXT_PUBLIC_API_URL}/files/${og.collectionId}/${og.id}/${og.og_image}`}
-          />
-          <meta
-            property="og:test"
-            content={`${process.env.NEXT_PUBLIC_API_URL}/files/${og.collectionId}/${og.id}/${og.og_image}`}
-          />
-        </Head>
+        <title>{og?.og_title}</title>
+        <meta property="og:title" content={og?.og_title} />
+        <meta
+          name="description"
+          content={og?.og_description.substring(0, 100)}
+        />
+        <meta
+          property="og:description"
+          content={og?.og_description.substring(0, 100)}
+        />
+        <meta
+          property="og:image"
+          content={`${process.env.NEXT_PUBLIC_API_URL}/files/${og.collectionId}/${og.id}/${og.og_image}`}
+        />
+        <meta
+          property="og:test"
+          content={`${process.env.NEXT_PUBLIC_API_URL}/files/${og.collectionId}/${og.id}/${og.og_image}`}
+        />
       </Head>
 
       <Carousel />
