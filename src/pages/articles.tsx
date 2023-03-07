a511c9fab18caea7770caef35c5e229c97b39e2e
azhari.subroto <azhari.subroto@gmail.com>
2023-03-07 18:47:16 +0700

    articles og

--- src/pages/articles.tsx
+++ src/pages/articles.tsx
@@ -55,7 +55,10 @@ const Articles: NextPage<any> = ({ og }) => {
       <Head>
         <title>{og?.og_title}</title>
         <meta property="og:title" content={og?.og_title} />
-        <meta name="description" content={og?.description.substring(0, 100)} />
+        <meta
+          name="description"
+          content={og?.og_description.substring(0, 100)}
+        />
         <meta property="og:description" content={og?.og_description} />
         <meta
           property="og:image"
