c39a056cabeb988551f5de29b6b6e748c7e2ba77
azhari.subroto <azhari.subroto@ottodigital.id>
2023-07-22 18:44:13 +0700

    status

--- src/pages/admin/products/edit/[id].tsx
+++ src/pages/admin/products/edit/[id].tsx
@@ -3,6 +3,9 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 import PocketBase from 'pocketbase';
 import { useEffect, useState } from 'react';
+import Switch from 'react-switch';
+
+import Button from '@/components/Button';
 
 const Editor = dynamic(() => import('@/components/Admin/Editor'), {
   ssr: false,
@@ -63,56 +66,81 @@ const ItemEdit = () => {
       </h2>
 
       {record && (
-        <div className="flex gap-3">
-          <div className="w-8/12">
-            {/* Title */}
-            <div className="mb-3">
-              <input
-                type="text"
-                name="title"
-                defaultValue={record.title}
-                value={record.title}
-                onChange={(e: any) => {
-                  setRecord({
-                    ...record,
-                    title: e.target.value,
-                  });
-                }}
-                className={inputStyle}
-              />
-            </div>
-
-            <div className="mb-3">
-              {typeof window !== 'undefined' && (
-                <Editor
-                  name="description"
-                  onChange={(data: any) => {
-                    // eslint-disable-next-line no-console
-                    console.log(data);
+        <form>
+          <div className="flex gap-3">
+            <div className="w-8/12">
+              {/* Title */}
+              <div className="mb-10">
+                <input
+                  type="text"
+                  name="title"
+                  defaultValue={record.title}
+                  value={record.title}
+                  onChange={(e: any) => {
+                    setRecord({
+                      ...record,
+                      title: e.target.value,
+                    });
                   }}
-                  editorLoaded={editorLoaded}
-                  value={
-                    record.description ? record.description : record.content
-                  }
+                  className={inputStyle}
                 />
-              )}
+              </div>
+
+              <div className="mb-3">
+                {typeof window !== 'undefined' && (
+                  <Editor
+                    name="description"
+                    onChange={(data: any) => {
+                      // eslint-disable-next-line no-console
+                      console.log(data);
+                    }}
+                    editorLoaded={editorLoaded}
+                    value={
+                      record.description ? record.description : record.content
+                    }
+                  />
+                )}
+              </div>
+            </div>
+            <div className="w-4/12">
+              <div className="mb-10">
+                <Button square variant="contained-blue">
+                  Save
+                </Button>
+              </div>
+
+              {/* Brands */}
+              <label htmlFor="brandId">Product Brand</label>
+              <select
+                id="brandId"
+                className="my-2 block w-full rounded-md border border-gray-300 bg-gray-100 px-3 py-2"
+                defaultValue={record.product_brand_id}
+              >
+                {brands?.map((item: any) => (
+                  <option key={`brand_id-${item.id}`} value={item.id}>
+                    {item.title}
+                  </option>
+                ))}
+              </select>
+
+              {/* Status */}
+              <div className="mt-6">
+                <label>Status</label>
+                <div className="mt-2">
+                  <Switch
+                    onChange={() => {
+                      setRecord({
+                        ...record,
+                        status: !record.status,
+                      });
+                    }}
+                    checked={record.status}
+                  />
+                </div>
+              </div>
             </div>
           </div>
-          <div className="w-4/12">
-            <label htmlFor="brandId">Product Brand</label>
-            <select
-              id="brandId"
-              className="my-2 block w-full rounded-md border border-gray-300 bg-gray-100 px-3 py-2"
-              value={record.product_brand_id}
-            >
-              {brands?.map((item: any) => (
-                <option key={`brand_id-${item.id}`} value={item.id}>
-                  {item.title}
-                </option>
-              ))}
-            </select>
-          </div>
-        </div>
+        </form>
       )}
 
       <pre>{JSON.stringify(record, null, 2)}</pre>
