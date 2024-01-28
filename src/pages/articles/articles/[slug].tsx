81046d391d41ff01a1d57a134b8a9c52f84c99df
malikdev13 <malik@inweb3.xyz>
2024-01-28 22:37:42 +0700

    add fixer in double articles path

--- src/pages/articles/articles/[slug].tsx
+++ src/pages/articles/articles/[slug].tsx
@@ -1,22 +1,21 @@
-import type { GetStaticPaths, GetStaticProps, NextPage } from 'next';
-import { useRouter } from 'next/router';
-import PocketBase from 'pocketbase';
+import type {
+  GetServerSideProps,
+  GetStaticPaths,
+  GetStaticProps,
+  NextPage,
+} from "next";
+import { useRouter } from "next/router";
 
-import BlogContent from '@/components/BlogContent';
+import BlogContent from "@/components/BlogContent";
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  return { paths: [], fallback: true };
-};
-
-export const getStaticProps: GetStaticProps = async (context) => {
-  const pb = new PocketBase(process.env.NEXT_PUBLIC_PB_URL);
+export const getServerSideProps: GetServerSideProps = async (context) => {
   const { slug } = context.params as any; // no longer causes error
-  const record = await pb
-    .collection('articles')
-    .getFirstListItem(`slug ~ "${slug}"`);
   return {
-    props: { og: JSON.parse(JSON.stringify(record)) },
-    revalidate: 60,
+    redirect: {
+      permanent: false,
+      destination: `/articles/${slug})}`,
+    },
+    props: {},
   };
 };
 
