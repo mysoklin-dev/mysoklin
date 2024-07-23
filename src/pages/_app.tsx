8382def961c7e02cb3faf7041740f70ed411ef05
Adhitia Darmawan <adhitiadarmawan@gmail.com>
2024-07-23 11:30:37 +0700

    [UPDATE] GTM tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/next-script-for-ga */
 import '../styles/global.css';
 
 import { Analytics } from '@vercel/analytics/react';
@@ -46,9 +47,18 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
       </div>
     </SWRConfig>
     {/* eslint-disable-next-line @next/next/next-script-for-ga */}
-    <script
+    {/* <script
       async
       src="https://www.googletagmanager.com/gtag/js?id=G-B0Z9CEQSB9"
+    ></script> */}
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KX6G8HFN')`,
+      }}
     ></script>
     <script
       dangerouslySetInnerHTML={{
