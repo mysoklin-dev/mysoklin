1e11800f2eac6361ab8969fbb1684ad2c2e13b02
Adhitia Darmawan <adhitiadarmawan@gmail.com>
2024-07-23 15:13:04 +0700

    [UPDATE] GTM TAG

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -47,8 +47,13 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
     </SWRConfig>
     {/* eslint-disable-next-line @next/next/next-script-for-ga */}
     <script
-      async
-      src="https://www.googletagmanager.com/gtag/js?id=G-B0Z9CEQSB9"
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KX6G8HFN')`,
+      }}
     ></script>
     <script
       dangerouslySetInnerHTML={{
