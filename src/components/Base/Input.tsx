e467ce22265f923fe7fb83e1b110f457310b2cb5
RSurya99 <rafli.rfsp@gmail.com>
2024-12-05 23:48:19 +0700

    feat: use forwardRef in base input component

--- src/components/Base/Input.tsx
+++ src/components/Base/Input.tsx
@@ -1,20 +1,25 @@
 import type { InputHTMLAttributes } from 'react';
-import React from 'react';
+import React, { forwardRef } from 'react';
 
 type TBaseInputProps = InputHTMLAttributes<HTMLInputElement> & {
   error?: string;
 };
 
-const BaseInput = (props: TBaseInputProps) => {
-  return (
-    <div className="flex flex-col">
-      <input
-        {...props}
-        className="h-fit w-full rounded-lg bg-white p-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
-      />
-      {props.error && <p className="text-red-500">{props.error}</p>}
-    </div>
-  );
-};
+const BaseInput = forwardRef<HTMLInputElement, TBaseInputProps>(
+  (props, ref) => {
+    return (
+      <div className="flex flex-col">
+        <input
+          {...props}
+          ref={ref}
+          className="h-fit w-full rounded-lg bg-white p-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        {props.error && <p className="text-red-500">{props.error}</p>}
+      </div>
+    );
+  }
+);
+
+BaseInput.displayName = 'BaseInput';
 
 export default BaseInput;
